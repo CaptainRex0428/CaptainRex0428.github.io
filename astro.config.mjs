8e761f020f9839fbdfaa98d780dbcaa274c00220
Luogongkai <rvo0428@163.com>
2026-05-27 11:32:18 +0800

    clear unused assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://captainrex0428.github.io',
   base: '/',
   integrations: [
     mdx(),
-    react(),
   ],
   i18n: {
     defaultLocale: 'zh',
